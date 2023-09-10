c77e92597db5829a8de395cc8542fb96e38edecb
Amrahvn <amrahvn@code.edu.az>
2023-09-11 00:31:28 +0400

    added

---
+++ LastBackProje/Program.cs
@@ -0,0 +1,27 @@
+using LastBackProje.DataAccesLAyer;
+using LastBackProje.Interfaces;
+using LastBackProje.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    //options.Cookie.Expiration = TimeSpan.FromSeconds(10);
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ILayoutService,LayoutService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseSession();
+app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
